2aea836ed91e1d294bded3b2465b607be6798210
Zarif School Projects <99420415+LordOfTheFrenchFries@users.noreply.github.com>
2024-02-28 19:29:24 -0700

    Update californiaAgriculture.js

--- californiaAgriculture.js
+++ californiaAgriculture.js
@@ -9,7 +9,7 @@ const text = document.getElementById("hoverTextCaption");
 
 function mouseOverFarm() {
   header.innerHTML = "Farming:";
-  text.innerHTML = "Farming was extremely profitable during the gold rush. Most farmed wheat and grapes, the former for bread and the latter for wine. Most farmers didn't initally plan on farming: they began as gold miners, but eventually for one reason or another, they decided to switch to farming. Later on, because of how many farmers there were, the price of wheat declined and farmers had to sell their land to make money. This price decrease lead to more crops, like lemons and oranges, being grown, once again making the farming rich. Farming was a major part of both California's economy and population boom."
+  text.innerHTML = "Farming was extremely profitable during the gold rush. Most farmed wheat and grapes, the former for bread and the latter for wine. Most farmers didn't initally plan on farming: they began as gold miners, but eventually for one reason or another, they decided to switch to farming. Later on, because of how many farmers there were, the price of wheat declined and farmers had to sell their land to make money. This price decrease lead to more crops, like lemons and oranges, being grown, once again making the farming rich. Farming was a major part of both California's economy and population boom.";
 }
 
 function mouseOutFarm() {
@@ -24,4 +24,5 @@ function mouseOverCow() {
 
 function mouseOutCow() {
   header.innerHTML = "Hover Over Something";
+  text.innerHTML = "";
 }
