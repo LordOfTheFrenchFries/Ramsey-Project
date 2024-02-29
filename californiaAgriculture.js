54d771118edfe398da821998758ad71cddd363a1
Zarif School Projects <99420415+LordOfTheFrenchFries@users.noreply.github.com>
2024-02-28 18:29:46 -0700

    Update californiaAgriculture.js

--- californiaAgriculture.js
+++ californiaAgriculture.js
@@ -3,3 +3,7 @@ document.getElementById("Farm").onmouseout = function() {mouseOutFarm()};
 
 document.getElementById("Cow").onmouseover = function() {mouseOverCow()};
 document.getElementById("Cow").onmouseout = function() {mouseOutCow()};
+
+const header = document.getElementById("
+
+function mouseOverFarm
