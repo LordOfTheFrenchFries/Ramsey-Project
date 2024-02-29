8d600ff29e4fe1ea972e8342eb21a95115855240
Zarif School Projects <99420415+LordOfTheFrenchFries@users.noreply.github.com>
2024-02-29 09:12:19 -0700

    Update indianWarTactics.js

--- indianWarTactics.js
+++ indianWarTactics.js
@@ -1,1 +1,7 @@
+document.getElementById("basic").onmouseover = function() {mouseOverFarm()};
+document.getElementById("basic").onmouseout = function() {mouseOutFarm()};
 
+document.getElementById("indian").onmouseover = function() {mouseOverCow()};
+document.getElementById("indain").onmouseout = function() {mouseOutCow()};
+
+const text = document.getElementById("");
