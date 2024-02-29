a734523bdda84c5ac64da77e59260f17d3f5defd
Zarif School Projects <99420415+LordOfTheFrenchFries@users.noreply.github.com>
2024-02-28 18:13:39 -0700

    Update californiaAgriculture.js

--- californiaAgriculture.js
+++ californiaAgriculture.js
@@ -1,7 +1,7 @@
 const farm = document.getElementById("Farm");
 const cow = document.getElementById("Cow");
 
-const header = document.getElementById("hoverBoxHeader");
+const header = document.getElementById("hoverTextHeader");
 
 // This handler will be executed only once when the cursor
 // moves over the unordered list
